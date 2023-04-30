01676066550f36ade279b81adb0ba6c7ebdbe5fb
amansindhal <jaswalgurmit@gmail.com>
2023-04-30 07:53:08 -0700

    dom modifying

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const yourName = 'Amandeep Kaur';
+newHeader.textContent = '${yourName} is the champion';
+
+const body = document.querySelector('body');
+body.appendChild(newHeader);
